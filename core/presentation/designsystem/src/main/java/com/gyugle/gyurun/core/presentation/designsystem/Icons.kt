5575d86c118d43b565f8d3a078d009d1c03b57c8
MinGyu Son <128052197+M1n9yu23@users.noreply.github.com>
2026-08-22 21:01:23 +0900

    feat(designsystem): 아이콘 및 공통 문자열 리소스 추가
    - 앱 전반에서 사용할 화살표, 동작 제어, 러닝 기록, 날씨 등 다양한
      Vector 아이콘 리소스를 추가함
    - Jetpack Compose 환경에서 아이콘을 편리하게 호출할 수 있도록
      `Icons.kt` 파일에 `ImageVector` 프로퍼티를 정의함
    - '뒤로 가기', '에러 알림', '다시 시도'와 같은 공통 UI 문자열을
      리소스 파일에 추가하고 한국어 다국어 처리를 지원함

---
+++ core/presentation/designsystem/src/main/java/com/gyugle/gyurun/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,77 @@
+package com.gyugle.gyurun.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+val ArrowBackIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_arrow_back)
+
+val ArrowUpIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_arrow_up)
+
+val PlayIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_play)
+
+val PauseIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_pause)
+
+val ErrorIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_error)
+
+val RunIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_run)
+
+val MapPinIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_map_pin)
+
+val StatsIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_stats)
+
+val DeleteIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_delete)
+
+val SettingsIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_settings)
+
+val ChevronRightIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevron_right)
+
+val OpenInNewIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_open_in_new)
+
+val WeatherClearIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_clear)
+
+val WeatherCloudyIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_cloudy)
+
+val WeatherFogIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_fog)
+
+val WeatherRainIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_rain)
+
+val WeatherSnowIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_snow)
+
+val WeatherThunderstormIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_weather_thunderstorm)
